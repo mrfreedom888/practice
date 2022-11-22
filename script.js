5b9680e8665d337111279c20a3b011a45ee1e9c6
mrfreedom888 <example@gmail.com>
2022-11-22 09:53:43 +0200

    something code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 'use strict'
 // const month = 12;
 
 // switch (month) {
@@ -122,44 +123,198 @@
 //     console.log(element);
 // }
 
-class Comment {
-    constructor(text){
-        this.text = text;
-        this.votesQty = 0
-    }
-    upvote(){
-        this.votesQty += 1;
-    }
-}
+// class Comment {
+//     constructor(text){
+//         this.text = text;
+//         this.votesQty = 0
+//     }
+//     upvote(){
+//         this.votesQty += 1;
+//     }
+// }
 
-const firstComment = new Comment ('First comment');
-const secondComment = new Comment ('Second comment');
-const thirdComment = new Comment ('Third comment');
+// const firstComment = new Comment ('First comment');
+// const secondComment = new Comment ('Second comment');
+// const thirdComment = new Comment ('Third comment');
 
 // console.log(firstComment instanceof Object || Comment);
 
-firstComment.upvote();
-console.log(firstComment.votesQty);
+// firstComment.upvote();
+// console.log(firstComment.votesQty);
 
-firstComment.upvote();
-console.log(firstComment.votesQty);
+// firstComment.upvote();
+// console.log(firstComment.votesQty);
 
-firstComment.upvote();
-console.log(firstComment.votesQty);
+// firstComment.upvote();
+// console.log(firstComment.votesQty);
 
-firstComment.upvote();
-console.log(firstComment.votesQty);
+// firstComment.upvote();
+// console.log(firstComment.votesQty);
 
-secondComment.upvote();
-console.log(secondComment.votesQty);
-
-
-secondComment.upvote();
-console.log(secondComment.votesQty);
+// secondComment.upvote();
+// console.log(secondComment.votesQty);
 
 
-secondComment.upvote();
-console.log(secondComment.votesQty);
+// secondComment.upvote();
+// console.log(secondComment.votesQty);
 
-thirdComment.upvote();
-console.log(thirdComment.votesQty);
+
+// secondComment.upvote();
+// console.log(secondComment.votesQty);
+
+// thirdComment.upvote();
+// console.log(thirdComment.votesQty);
+
+
+// class Comment {
+//     constructor(text){
+//         this.text = text;
+//         this.votesQty = 0
+//     }
+//     upvote(){
+//         this.votesQty += 1;
+//     }
+//     static mergeComments (first, second) {
+//         return `${first} ${second}`
+//     }
+// }
+
+// console.log(Comment.mergeComments('First comment.', 'Second comment.'));
+
+// class NumbersArray extends Array {
+//     sum() {
+//         return this.reduce((el, acc) => acc += el, 0)
+//     }
+// }
+
+// const myArray = new NumbersArray(2, 5, 7)
+
+// console.log(myArray)
+// console.log(myArray.sum());
+
+
+// const myPromise = new Promise ((resolve, reject) => {
+     
+// });
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+//     .catch(error => console.error(error.message));
+
+
+// const getData = (url) => 
+//     new Promise ((resolve, reject) => 
+//         fetch(url)
+//             .then(response => response.json())
+//             .then(json => resolve(json))
+//             .catch(error => reject(error))
+//     )
+
+// getData('https://jsonplaceholder.typicode.com/todos/3')
+//             .then(data => console.log(data))
+//             .catch(error => console.log(error.massage))
+
+
+// const asyncFn = async() => {
+//     throw new Error('seccess!');
+// } 
+
+// asyncFn()
+//     .then(value => console.log(value))
+//     .catch(error => console.log(error.message));
+
+// const timerPromise = () => 
+//     new Promise((resolve, reject) => 
+//         setTimeout(() => resolve(), 2000));
+
+// const asyncFn = async() => {
+//     console.log('timer starts');
+//     const startTime = performance.now();
+//     await timerPromise()
+//     const endTime = performance.now()
+//     console.log('timer ended', endTime - startTime);
+// }
+
+// asyncFn()
+
+// const getData = async (url) => {
+//     const res = await fetch(url);
+//     const json = res.json();
+//     return json
+// }
+
+// const url = 'https://jsonplaceholder.typicode.com/todos';
+// try{
+//     const data = await getData(url);
+//     console.log(data);
+// } catch(error){
+//     console.log(error.message);
+// }
+
+
+
+// function mostCommon(a, b) {
+//   const A = a.split('');
+//   const B = b.split('');
+//   const min = {
+//     diff: A.length + B.length,
+//     index: -A.length,
+//     start: undefined,
+//     finish: undefined,
+//   };
+
+//   for (let offset = -A.length; offset < B.length; offset++) {
+//     let diff = Math.max(0, -offset) + Math.max(offset + A.length - B.length, 0);
+//     const initialDiff = Math.max(0, -offset) + Math.max(offset + A.length - B.length, 0);
+//     const start = Math.min(Math.max(0, offset), B.length);
+//     const finish = Math.min(Math.max(0, offset + A.length), B.length);
+//     let matchStart;
+//     let matchFinish;
+//     for (let i = start, isMatchStarted = false; i < finish; i++) {
+//       if (B[i] !== A[i - offset]) diff++;
+//       else {
+//         if (!isMatchStarted) {
+//           matchStart = i;
+//           isMatchStarted = true;
+//         }
+//         matchFinish = i;
+//       }
+//     }
+
+//     if (diff < min.diff) {
+//       min.diff = diff;
+//       min.index = offset;
+//       min.start = matchStart;
+//       min.finish = matchFinish;
+//     }
+//   }
+
+//   console.log(min, b.substring(min.start, min.finish + 1));
+
+//   return min;
+// }
+
+// mostCommon('xxx>abcABCxxx', 'bbbzzz>abcAXCiiiqqq');
+
+
+let userName;
+do{
+    userName = prompt('What is your name?' ,'Oleh');
+}while( userName === null || !userName.trim())
+
+
+let userAge = +prompt('How old are you?' , 18);
+
+if (userAge < 18) {
+    alert('You are not allowed to visit this website');
+} else if ( userAge === 18 || userAge <= 22) {
+    let userAction = confirm('Are you sure you want to continue?');
+    if ( userAction === true){
+        alert(`Welcome, ${userName}`);
+    } else{
+        alert('You are not allowed to visit this website');
+    }
+}  else{
+    alert(`Welcome, ${userName}`);
+}
